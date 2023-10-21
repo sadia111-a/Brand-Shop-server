c1aa0b39b61a4153d650b22a99a60a729b1ff31d
sadia11-a <sadiasorna96@gmail.com>
2023-10-21 21:28:04 +0600

    delete operation for cart product done here

--- index.js
+++ index.js
@@ -80,6 +80,13 @@ async function run() {
       );
     });
 
+    app.delete("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
